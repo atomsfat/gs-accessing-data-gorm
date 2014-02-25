2d338395087b6ef17db7bb84f330598f3c6ceefa
Graeme Rocher <graeme.rocher@gmail.com>
2014-02-25 09:47:41 +0100

    tweaks to the groovy script example

--- script/app.groovy
+++ script/app.groovy
@@ -1,10 +1,8 @@
 @Grab("org.grails:gorm-hibernate4-spring-boot:1.0.0.M1")
 @Grab("com.h2database:h2:1.3.173")
-
 import grails.persistence.*
 import org.springframework.http.*
 import static org.springframework.web.bind.annotation.RequestMethod.*
-
 
 @RestController
 class GreetingController {
@@ -16,15 +14,10 @@ class GreetingController {
     }
 
     @RequestMapping(value = '/person/add', method = POST)
-    ResponseEntity addPerson(String firstName, String lastName) {
+    ResponseEntity addPerson(String firstName) {
         Person.withTransaction {
-            def p = new Person(firstName: firstName, lastName: lastName).save()
-            if(p) {
-                return new ResponseEntity(HttpStatus.CREATED)
-            }
-            else {
-                return new ResponseEntity(HttpStatus.BAD_REQUEST)
-            }
+            def p = new Person(firstName: firstName).save()
+            return new ResponseEntity( p ? HttpStatus.CREATED : HttpStatus.BAD_REQUEST)
         }
     }
 }
